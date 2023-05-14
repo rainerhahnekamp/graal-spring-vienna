ea6f4392b32d3b1302cc4255ee55c5a16cfadf01
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-14 21:09:30 +0200

    init

---
+++ src/main/java/com/example/graalspringvienna/GraalSpringViennaApplication.java
@@ -0,0 +1,13 @@
+package com.example.graalspringvienna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalSpringViennaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraalSpringViennaApplication.class, args);
+    }
+
+}
